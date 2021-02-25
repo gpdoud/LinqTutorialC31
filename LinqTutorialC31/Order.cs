6331c173f1fe234664b781d79de2321b96d5200c
Greg Doud <gpdoud@gmail.com>
2021-02-25 15:56:42 -0500

    Add project files.

---
+++ LinqTutorialC31/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTutorialC31 {
+    class Order {
+
+        public int Id { get; set; }
+        public decimal Total { get; set; }
+        public int CustId { get; set; }
+
+    }
+}
---
+++ LinqTutorialC31/Orderline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTutorialC31 {
+    class Orderline {
+
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
